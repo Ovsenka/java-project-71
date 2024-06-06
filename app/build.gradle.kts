99f2e56dc4fdd698f8dce9959d25b7fb01babdf3
Ovsenka <ovsenka3@gmail.com>
2024-06-06 16:24:01 +0300

    gendiff usage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    implementation("info.picocli:picocli:4.7.6")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,24 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
 
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.",
+        version = "gendiff 0.1"
+        )
+public class App implements Callable<Integer> {
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("Call()");
+        return 0;
     }
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
 }
