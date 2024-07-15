df97d4cd4a19663728d5b0f5e50831fad5de7e47
Ovsenka <ovsenka3@gmail.com>
2024-07-15 17:17:19 +0300

    differ

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Ovsenka/java-project-71/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Ovsenka/java-project-71/actions)
+
+#### Differ demo:
+[![asciicast](https://asciinema.org/a/668134.svg)](https://asciinema.org/a/668134)
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -6,9 +6,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 
 public class Differ {
     public static String generate(Path filepath, Path filepath2) throws IOException {
@@ -18,22 +21,28 @@ public class Differ {
                 new TypeReference<Map<String, Object>>() {});
         Map<String, Object> mapFile2 = mapper.readValue(new File(filepath2.toString()),
                 new TypeReference<Map<String, Object>>() {});
-        for (Entry entry : mapFile.entrySet()) {
-            String key = entry.getKey().toString();
-            Object value = entry.getValue();
+        SortedSet<String> keys = new TreeSet<>(mapFile.keySet());
+        for (String key : keys) {
+            Object value = mapFile.get(key);
             if (mapFile2.containsKey(key)) {
                 Object differFileKeyValue = mapFile2.get(key);
                 if (differFileKeyValue.equals(value)) {
                     differStr.append("\n\t" + key + ": " + value);
                 }
                 else {
-                    differStr.append("\n\t- " + key + ": " + entry.getValue().toString());
-                    differStr.append("\n\t+ " + key + ": " + mapFile2.get(key));
+                    differStr.append("\n\t- " + key + ": " + value.toString());
+                    differStr.append("\n\t+ " + key + ": " + differFileKeyValue);
                 }
+                mapFile2.remove(key);
             } else {
-                // ...
+                differStr.append("\n\t- " + key + ": " + value.toString());
             }
         }
+        mapFile2.forEach((k, v) -> {
+            if (!mapFile.containsKey(k)) {
+                differStr.append("\n\t+ " + k + ": " + v);
+            }
+        });
         differStr.append("\n}");
         return differStr.toString();
     }
