e8b85289d71464c4d141450437bde120f818adbb
Ovsenka <ovsenka3@gmail.com>
2024-09-23 00:49:47 +0300

    refactor

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ### Hexlet tests and linter status:
-[![Actions Status](https://github.com/Ovsenka/java-project-71/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Ovsenka/java-project-71/actions)
+[![Java CI](https://github.com/Ovsenka/java-project-71/actions/workflows/gradle.yml/badge.svg?branch=main)](https://github.com/Ovsenka/java-project-71/actions/workflows/gradle.yml)
+
 
 #### Differ demo:
 [![asciicast](https://asciinema.org/a/668134.svg)](https://asciinema.org/a/668134)
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -6,44 +6,47 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 
 public class Differ {
     public static String generate(Path filepath, Path filepath2) throws IOException {
-        StringBuilder differStr = new StringBuilder("{");
         ObjectMapper mapper = new ObjectMapper();
         Map<String, Object> mapFile = mapper.readValue(new File(filepath.toString()),
-                new TypeReference<Map<String, Object>>() {});
+                new TypeReference<>() {});
         Map<String, Object> mapFile2 = mapper.readValue(new File(filepath2.toString()),
-                new TypeReference<Map<String, Object>>() {});
+                new TypeReference<>() {});
+        return compareFilesProperties(mapFile, mapFile2);
+    }
+
+    private static String compareFilesProperties(Map<String, Object> mapFile, Map<String, Object> mapFile2) {
+        StringBuilder differStr = new StringBuilder("{");
         SortedSet<String> keys = new TreeSet<>(mapFile.keySet());
         for (String key : keys) {
             Object value = mapFile.get(key);
             if (mapFile2.containsKey(key)) {
                 Object differFileKeyValue = mapFile2.get(key);
                 if (differFileKeyValue.equals(value)) {
-                    differStr.append("\n\t" + key + ": " + value);
+                    differStr.append("\n\t").append(key).append(": ").append(value);
                 }
                 else {
-                    differStr.append("\n\t- " + key + ": " + value.toString());
-                    differStr.append("\n\t+ " + key + ": " + differFileKeyValue);
+                    differStr.append("\n\t- ").append(key).append(": ").append(value.toString());
+                    differStr.append("\n\t+ ").append(key).append(": ").append(differFileKeyValue);
                 }
                 mapFile2.remove(key);
             } else {
-                differStr.append("\n\t- " + key + ": " + value.toString());
+                differStr.append("\n\t- ").append(key).append(": ").append(value.toString());
             }
         }
         mapFile2.forEach((k, v) -> {
             if (!mapFile.containsKey(k)) {
-                differStr.append("\n\t+ " + k + ": " + v);
+                differStr.append("\n\t+ ").append(k).append(": ").append(v);
             }
         });
         differStr.append("\n}");
         return differStr.toString();
     }
+
 }
